01dadd643a5a7bced150f6b485f2db5ff9a0ebbc
ErikCodeGit <erik.mueller0912@gmail.com>
2023-02-25 10:28:32 +0100

    Add functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  root 'posts#index'
+
+  resources :posts, only: %i[new create index]
+  resources :users, only: %i[new create edit update]
 end
